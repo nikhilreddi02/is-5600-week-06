9da21065009e736edc7302af09c02d6b9a25f07e
nikhilreddi02 <naveendharshanala143@gmail.com>
2026-04-26 11:53:48 -0500

    Intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+
+// App.jsx
+import Header from './components/Header';
+
 import productData from './data/full-products';
 
+// App.jsx
+import { Route, Routes} from 'react-router-dom';
+
+// ...
 function App() {
   return (
     <div className="App">
-      <h1>Hello There.</h1>
+      <Header />
+      
+        <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
+      
     </div>
   );
 }
-
-export default App;
+ 
